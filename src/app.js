01afe735bebe8f817df52a57bd90d1a7e6139089
emeka <emekaofe22@gmail.com>
2021-08-27 15:39:29 +0100

    add eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,31 +1,29 @@
-import express from 'express'
+import express from 'express';
 import cors from 'cors';
 // require('express-async-errors');
 
-//I'm using require to remove morgan warning error when using import and esm
-const morgan = require('morgan')
-const app = express()
+// I'm using require to remove morgan warning error when using import and esm
+const morgan = require('morgan');
+
+const app = express();
 
 const corsOptions = {
   credentials: true,
   origin: [],
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-
-app.use(cors(corsOptions))
-app.use(morgan('combined'))
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(cors(corsOptions));
+app.use(morgan('combined'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // routes
 app.get('/', (req, res) => res.status(301).redirect('/api/v1'));
-app.all('*', (req, res) => {
-  throw new ApplicationError({
-    status: 404,
-    message: `Can't find route [ ${req.originalUrl} ] on this server`,
-  });
-});
+app.all('*', (req, res) => res.status(404).send({
+  status: 404,
+  message: `Can't find route [ ${req.originalUrl} ] on this server`,
+}));
 // app.use(errorHandler);
 
-export default app
+export default app;
