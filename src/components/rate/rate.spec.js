b449381ed6afe00239e5410859a530aa03f1bf0f
emeka <emekaofe22@gmail.com>
2021-08-29 13:52:22 +0100

    update main

--- src/components/rate/rate.spec.js
+++ src/components/rate/rate.spec.js
@@ -18,22 +18,4 @@ describe('Rate Routes', () => {
       response.body.message.should.equal("Can't access route, please make sure you are logged in");
     });
   });
-  //   context('Search Routes', () => {
-  //     const user = { email: 'emekaofe2@gmail.com', password: '$argon2i$v=19$m=16,t=2,p=1$aGVsbG93b3JsZA$xpGMS+PyJyBLOQj6x1UiyA' };
-  //     before(async () => {
-  //       await User.create(user);
-  //       const db = await request(app)
-  //         .post('/api/v1/user/signin')
-  //         .send({ email: 'emekaofe2@gmail.com', password: 'maths@1000' });
-  //     });
-
-//     it('should throw when a user that not logged in tries to rate a book item', async () => {
-//       const response = await request(app)
-//         .post(`${BASE_ROUTE}/${bookSlug}`);
-//       console.log(response.body);
-//       response.statusCode.should.equal(200);
-//     //   response.body.success.should.equal(false);
-//     //   response.body.message.should.equal("Can't access route, please make sure you are logged in");
-//     });
-//   });
 });
